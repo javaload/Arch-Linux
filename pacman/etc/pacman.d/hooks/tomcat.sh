b230536a92e2a55b0c34b318a2091329e549ef1b
vi_six <mrgild@gmail.com>
2017-09-07 17:29:39 +0300

    Pacman post-install and post-upgrade tomcat hook. Hook designed to modify access rights to updated or installed tomcat7 or tomcat8 package in order to run java applications under local tomcat without troubles.

---
+++ pacman/etc/pacman.d/hooks/tomcat.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# This script modifies access right to tomcat filders.
+
+sudo chmod o+rwx /usr/share/tomcat8/* -R
+sudo chmod o+rwx /var/lib/tomcat8/* -R
+sudo chmod o+rwx /etc/tomcat8/* -R
+
+sudo chmod o+rwx /usr/share/tomcat7/* -R
+sudo chmod o+rwx /var/lib/tomcat7/* -R
+sudo chmod o+rwx /etc/tomcat7/* -R
+
+echo "tomcat7 and tomcat8 folders access rights set to o+rwx"
